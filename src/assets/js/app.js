397c5ca5bfbc945b91487cef29b24617fcc88978
Lea <lealondero2@gmail.com>
2022-03-11 10:33:32 +0100

    js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,5 @@
-let old = document.querySelector(".card:first-of-type");
-document.querySelectorAll(".card").forEach((card) => {
+let old = document.querySelector(".novels__card:first-of-type");
+document.querySelectorAll(".novels__card").forEach((card) => {
   card.addEventListener("click", () => {
     console.log("ok");
     if (old != null && old.classList.contains("open")) {
